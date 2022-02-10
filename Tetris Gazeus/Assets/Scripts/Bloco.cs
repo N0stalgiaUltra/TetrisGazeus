78ceaa05b835920199d6373e988056c339db6282
Vinicius Santos <viniciusantos0898@gmail.com>
2022-02-10 09:04:14 -0300

    Setup do projeto e adição de movimento para o bloco

---
+++ Tetris Gazeus/Assets/Scripts/Bloco.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloco : MonoBehaviour
+{
+
+    private Vector2 movimento;
+
+    [SerializeField] private Rigidbody2D rb;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetButtonDown("Rotate"))
+            Rotation();
+
+    }
+
+
+    private void Rotation()
+    {
+        print("rotacionando o objeto");
+    }
+}
