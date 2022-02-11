86629692c494a08406ef97870d0c895bed168c09
Vinicius Santos <viniciusantos0898@gmail.com>
2022-02-10 21:06:34 -0300

    Scripts de Managers adicionados; Prefabs dos Blocos Adicionados

--- Tetris Gazeus/Assets/Scripts/Bloco.cs
+++ Tetris Gazeus/Assets/Scripts/Bloco.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 public class Bloco : MonoBehaviour
 {
 
-    private Vector2 movimento;
-
+    [Header("Referencias")]
     [SerializeField] private Rigidbody2D rb;
+    
 
     void Start()
     {
-        
     }
 
     void Update()
     {
-        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        //dessa forma o movimento fica mais proximo do tetris de console
+        if(InBound())
+        {
+            //this.transform.position -= new Vector3(0, 1, 0);
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+                transform.position += new Vector3(1, 0, 0);
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+                transform.position += new Vector3(-1, 0, 0);
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+                transform.position += new Vector3(0, -1, 0);
+        }
+
+        
+
 
         if (Input.GetButtonDown("Rotate"))
             Rotation();
@@ -27,5 +42,21 @@ public class Bloco : MonoBehaviour
     private void Rotation()
     {
         print("rotacionando o objeto");
+    }
+
+    /// <summary>
+    /// Retorna se o bloco está dentro do limite da grid
+    /// </summary>
+    /// <returns></returns>
+    private bool InBound()
+    {
+        //Checa se cada "bloquinho" que compõe um bloco não extrapolou os limites do grid
+        foreach (Transform childTransform in gameObject.transform)
+        {
+            if (childTransform.position.x >= (GridManager.instance.GridLargura / 2) || childTransform.position.x <= -(GridManager.instance.GridLargura / 2)|| childTransform.position.y < -(GridManager.instance.GridAltura/2))
+                return false;
+        }
+
+        return true;
     }
 }
---
+++ Tetris Gazeus/Assets/Scripts/GridManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    #region Singleton
+    public static GridManager instance;
+    #endregion
+
+
+    [Header("Info da Grid")]
+    [SerializeField] private int altura;
+    [SerializeField] private int largura;
+
+    public static int[,] grid;
+
+    private void Awake()
+    {
+        //declarando Singleton
+        if (instance != null)
+            Destroy(this.gameObject);
+        else
+            instance = this;
+
+    }
+
+    private void Update()
+    {
+    }
+    private void CleanLines()
+    {
+        //loopa a grid na altura
+            //checa se há alguma linha == null
+                //se tiver: deleta todos os objs
+                    // chama metodo pra descer a linha
+                //senão, i++
+    }
+
+    private void DropLines()
+    {
+        //loopa a grid pela altura
+            //checa qual a linha que ficou null
+            //pega a linha acima e desce
+            //faz isso para todas as linhas
+    }
+    #region Propriedades
+    public int GridAltura { get => altura; }
+    public int GridLargura { get => largura; }
+    #endregion
+}
