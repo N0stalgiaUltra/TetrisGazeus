e71849f22d5ac69bc889c16e04424245c441abe0
Vinicius Santos <viniciusantos0898@gmail.com>
2022-02-11 19:07:10 -0300

    Adicionando métodos para controlar a grid

--- Tetris Gazeus/Assets/Scripts/Bloco.cs
+++ Tetris Gazeus/Assets/Scripts/Bloco.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +8,14 @@ public class Bloco : MonoBehaviour
 
     [Header("Referencias")]
     [SerializeField] private Rigidbody2D rb;
-
     [SerializeField] private float time;
+    private static Transform[,] grid;
 
     void Start()
     {
         time = 0;
+
+        
     }
 
     void Update()
@@ -31,14 +34,18 @@ public class Bloco : MonoBehaviour
             }
             if (Input.GetKeyDown(KeyCode.RightArrow))
                 transform.position += new Vector3(1, 0, 0);
+            
             else if (Input.GetKeyDown(KeyCode.LeftArrow))
                 transform.position += new Vector3(-1, 0, 0);
+            
             else if (Input.GetKeyDown(KeyCode.DownArrow))
                 transform.position += new Vector3(0, -1, 0);
         }
 
-        
 
+
+        if (Input.GetKeyDown(KeyCode.A))
+            AddBlock();
 
         if (Input.GetButtonDown("Rotate"))
             Rotation();
@@ -57,13 +64,31 @@ public class Bloco : MonoBehaviour
     /// <returns></returns>
     private bool InBound()
     {
-        //Checa se cada "bloquinho" que compõe um bloco não extrapolou os limites do grid
         foreach (Transform childTransform in gameObject.transform)
         {
             if (childTransform.position.x >= (GridManager.instance.GridLargura / 2) || childTransform.position.x <= -(GridManager.instance.GridLargura / 2)|| childTransform.position.y < -(GridManager.instance.GridAltura/2))
                 return false;
+            
+
         }
 
         return true;
+    }
+
+    /// <summary>
+    /// Salva a posição do bloco em uma grid 
+    /// </summary>
+    private void AddBlock()
+    {   
+        //adiciona cada bloco a uma grid de transform, ou seja, adiciona ao grid a posição de um bloco
+        foreach (Transform childTransform in gameObject.transform)
+        {
+            int x = (int)Math.Round(childTransform.position.x);
+            int y = (int)Math.Round(childTransform.position.y);
+
+            if (grid[x, y] == null) //checa se não há nenhum outro objeto ocupando a mesma pos
+                grid[x, y] = childTransform;
+
+        }
     }
 }
--- Tetris Gazeus/Assets/Scripts/GridManager.cs
+++ Tetris Gazeus/Assets/Scripts/GridManager.cs
@@ -13,7 +13,7 @@ public class GridManager : MonoBehaviour
     [SerializeField] private int altura;
     [SerializeField] private int largura;
 
-    public static int[,] grid;
+    public static Transform[,] grid = new Transform[instance.largura, instance.altura];
 
     private void Awake()
     {
@@ -23,27 +23,70 @@ public class GridManager : MonoBehaviour
         else
             instance = this;
 
-    }
 
+    }
+    
     private void Update()
     {
     }
-    private void CleanLines()
+
+    /// <summary>
+    /// Limpa as linhas completadas, resetando-as
+    /// </summary>
+    /// <param name="alt"> Altura da grid a ser resetada </param>
+    public void CleanLines(int alt)
     {
-        //loopa a grid na altura
-            //checa se há alguma linha == null
-                //se tiver: deleta todos os objs
-                    // chama metodo pra descer a linha
-                //senão, i++
+        for (int i = 0; i < largura; i++)
+        {
+            Destroy(grid[largura, alt].gameObject);
+            grid[largura, alt] = null;
+        }
+        
     }
 
-    private void DropLines()
+    /// <summary>
+    /// Desce as linhas acima daquela que foi resetada
+    /// </summary>
+    /// <param name="altura">Altura da grid acima da que foi resetada</param>
+    public void DropLines(int alt)
     {
-        //loopa a grid pela altura
-            //checa qual a linha que ficou null
-            //pega a linha acima e desce
-            //faz isso para todas as linhas
+        for (int i = 0; i < largura; i++)
+        {
+            if(grid[largura,alt] != null)
+            {
+                grid[largura, alt - 1] = grid[largura, alt];
+                grid[largura, alt] = null;
+                grid[largura, alt - 1].position += new Vector3(0,-1,0);
+            }
+        }
     }
+    /// <summary>
+    /// Checa se as linhas estão preenchidas
+    /// </summary>
+    /// <param name="alt">altura da grid</param>
+    /// <returns>se a linha está preenchida</returns>
+    private bool FullLines(int alt)
+    {
+        for (int i = 0; i < largura; i++)
+            if (grid[largura, alt] != null)
+                return true;
+        return false;
+    }
+
+    public void LineControl()
+    {
+        for (int i = 0; i < instance.altura; i++)
+        {
+            if(FullLines(i))
+            {
+                CleanLines(i);
+                for (int j = instance.altura; j < instance.altura; j++) //loopa para pegar as linhas acima da deletada e descer elas
+                    DropLines(i);
+            }
+                
+        }
+    }
+
     #region Propriedades
     public int GridAltura { get => altura; }
     public int GridLargura { get => largura; }
