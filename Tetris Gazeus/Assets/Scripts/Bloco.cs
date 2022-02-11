5238527d99cd6749d0f897e6670cc1ade60395a5
Vinicius Santos <viniciusantos0898@gmail.com>
2022-02-10 21:36:24 -0300

    Adicionado movimento para os blocos (WIP)

--- Tetris Gazeus/Assets/Scripts/Bloco.cs
+++ Tetris Gazeus/Assets/Scripts/Bloco.cs
@@ -7,21 +7,28 @@ public class Bloco : MonoBehaviour
 
     [Header("Referencias")]
     [SerializeField] private Rigidbody2D rb;
-    
+
+    [SerializeField] private float time;
 
     void Start()
     {
+        time = 0;
     }
 
     void Update()
     {
         //rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
+        time += Time.deltaTime;
         //dessa forma o movimento fica mais proximo do tetris de console
         if(InBound())
         {
-            //this.transform.position -= new Vector3(0, 1, 0);
-
+            //a cada 0.5s, um bloco cai 
+            if (time > .5f)
+            {
+                this.transform.position -= new Vector3(0, 1, 0);
+                time = 0;
+            }
             if (Input.GetKeyDown(KeyCode.RightArrow))
                 transform.position += new Vector3(1, 0, 0);
             else if (Input.GetKeyDown(KeyCode.LeftArrow))
